89725f6dd121f998840b57e540e08346759b5bb7
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-11-19 17:32:08 +0300

    completed responsiveness for navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,63 +1,75 @@
 import React, { useContext, useState } from "react";
 import logo from "../Assets/Untitled.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
+  const [isToggle, setIsToggle] = useState(false);
+
+  const handleClick = () => {
+    setIsToggle(!isToggle);
+  };
 
   return (
     <div className="flex shadow justify-around">
-      <div className="flex items-center">
+      <div className="flex z-10 items-center">
+        <FontAwesomeIcon
+          className="md:hidden bg-white cursor-pointer text-[24px] absolute left-[20px]"
+          icon={isToggle ? faBars : faX}
+          onClick={handleClick}
+        />
         <img className="w-[60px] h-[60px]" src={logo} alt="logo" />
         <p className="text-slate-950 text-2xl uppercase font-semibold">
           Oningoi
         </p>
       </div>
-      <ul className="flex nav-menu items-center gap-5 text-neutral-600 font-medium text-xl">
-        <li
-          onClick={() => {
-            setMenu("shop");
-          }}
-          className="flex flex-col items-center content-center gap-2 cursor-pointer"
-        >
-          <Link to="/">Shop</Link>
-          {menu === "shop" ? <hr /> : <></>}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("phone");
-          }}
-          className="flex flex-col items-center content-center gap-2 cursor-pointer"
-        >
-          <Link to="/phone">Phone</Link>
-          {menu === "phone" ? <hr /> : <></>}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("laptop");
-          }}
-          className="flex flex-col items-center content-center gap-2 cursor-pointer"
-        >
-          <Link to="/laptop">Laptop</Link>
-          {menu === "laptop" ? <hr /> : <></>}
-        </li>
-        <li
-          onClick={() => {
-            setMenu("tablet");
-          }}
-          className="flex flex-col items-center content-center gap-2 cursor-pointer"
-        >
-          <Link to="/tablet">Tablet</Link>
-          {menu === "tablet" ? <hr /> : <></>}
-        </li>
+      <ul className={`absolute ${isToggle ? 'hidden' : ''} shadow-2xl md:relative md:h-[0px] md:w-[0px] md:bg-[transparent]   bg-white h-[300px] w-[250px]  top-[0px] left-[0px]`}>
+        <div className="md:flex md:block top-[60px] md:top-[0px] grid md:left-[0px] left-[50px] absolute md:relative nav-menu items-center gap-5 text-neutral-600 font-medium text-xl">
+          <li
+            onClick={() => {
+              setMenu("shop");
+            }}
+            className="flex flex-col md:items-center content-center gap-2 cursor-pointer"
+          >
+            <Link to="/">Shop</Link>
+            {menu === "shop" ? <hr /> : <></>}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("phone");
+            }}
+            className="flex flex-col md:items-center content-center gap-2 cursor-pointer"
+          >
+            <Link to="/phone">Phone</Link>
+            {menu === "phone" ? <hr /> : <></>}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("laptop");
+            }}
+            className="flex flex-col md:items-center content-center gap-2 cursor-pointer"
+          >
+            <Link to="/laptop">Laptop</Link>
+            {menu === "laptop" ? <hr /> : <></>}
+          </li>
+          <li
+            onClick={() => {
+              setMenu("tablet");
+            }}
+            className="flex flex-col md:items-center content-center gap-2 cursor-pointer"
+          >
+            <Link to="/tablet">Tablet</Link>
+            {menu === "tablet" ? <hr /> : <></>}
+          </li>
+        </div>
       </ul>
       <div className="flex items-center gap-4">
         <Link to="/login">
-          <button className="border border-solid w-[100px] h-[30px] font-semibold text-xl text-center rounded-full bg-white border-slate-500 active:bg-slate-200">
+          <button className={`border  ${isToggle ? 'hidden' : ''} md:relative top-[250px] left-[50px] absolute left border-solid w-[100px] h-[30px] font-semibold text-xl text-center rounded-full bg-white border-slate-500 active:bg-slate-200`}>
             Login
           </button>
         </Link>
