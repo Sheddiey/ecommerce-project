a05b202c64563b6ee8f3853f0003bfa170fb7593
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-11-19 17:41:19 +0300

    added responsiveness to the banner component

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,11 +3,11 @@ import banner_img from '../Assets/banner-image.jpg'
 
 const Banner = () => {
   return (
-    <div className="flex justify-between p-[20px] overflow-hidden mt-[10px] bg-radial h-[30vh] w-[87%] m-[auto] mb-[20px]">
+    <div className="md:flex grid gap-[20px] justify-between p-[20px] mt-[10px] bg-radial md:h-[30vh] w-[87%] m-[auto] mb-[20px]">
       <div className="grid gap-[10px]">
         <p className="uppercase text-3xl text-[#ff4141]">Best Offer Flat 50% Off</p>
         <p>12 Hours 20 Mins</p>
-        <button className="w-[260px] px-[10px] rounded-[33px] text-white bg-red-500 text-[22px] font-medium cursor-pointer">Explore now</button>
+        <button className="w-[260px] h-[60px] px-[10px] rounded-[33px] text-white bg-red-500 text-[22px] font-medium cursor-pointer">Explore now</button>
       </div>
       <div className="">
         <img className="h-[100%] w-[300px]" src={banner_img} alt="" />
